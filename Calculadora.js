71930f7e07b6da141b477daf25995f4b0c0442cb
maurimdn <neipanmauricio@gmail.com>
2021-08-31 15:56:50 -0300

    first commit

---
+++ Calculadora.js
@@ -0,0 +1,18 @@
+// declaramos la clase para las operaciones
+class Calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+} 
